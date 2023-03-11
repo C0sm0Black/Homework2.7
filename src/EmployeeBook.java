ee5571be254fe878de2a83a5f23cd956cd3b2799
CosmoBlack <soulker_mozyr@yahoo.com>
2023-03-11 15:58:01 +0200

    Task 1

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,94 +1,83 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EmployeeBook {
 
-    private final Employee[] employees;
-    private int count;
 
-    public EmployeeBook(int size) {
-        this.employees = new Employee[size];
+    private final Map<String, Employee> employees;
+
+    public EmployeeBook() {
+        this.employees = new HashMap<>();
     }
 
     public void addEmployee(Employee employee) {
 
-        if (employee == null) {
+        String s = employee.getFirstName() + employee.getMiddleName() + employee.getLastName();
 
-            System.out.println("Значение сотрудника рано null!");
+
+        if (employees.containsKey(s)) {
+
+            System.out.println("Такой сотрудник уже есть");
             return;
         }
 
-        if (count == employees.length) {
-
-            System.out.println("Нет места для добавления нового сотрудника!");
-            return;
-
-        }
-
-        this.employees[count] = employee;
-        count++;
+        employees.put(s, employee);
 
     }
 
     public void removeEmployee(Employee employee) {
 
-        int index = -1;
+        String s = employee.getFirstName() + employee.getMiddleName() + employee.getLastName();
 
-        for (int i = 0; i < count; i++) {
+        if (employees.containsKey(s)) {
 
-            if (employee.equals(this.employees[i])) {
-
-                index = i;
-                break;
-            }
-
-        }
-
-        if (index == -1) {
-
-            System.out.println("Данного сотрудника нет");
+            employees.remove(s);
             return;
-
         }
 
-        System.arraycopy(this.employees, index + 1, this.employees, index, count - 1 - index);
-        count--;
+        System.out.println("Такого сотрудника нет!");
+
     }
 
     public void printEmployees() {
 
-        for (int i = 0; i < count; i++) {
-            System.out.println(this.employees[i]);
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
+            System.out.println(pair.getValue().toString());
         }
 
     }
 
     public void changeSalary(String firstName, String middleName, String lastName, double newSalary) {
 
-        for (int i = 0; i < count; i++) {
+        String s = firstName + middleName + lastName;
 
-            if (this.employees[i].getFirstName().equals(firstName) && this.employees[i].getMiddleName().equals(middleName)
-                    && this.employees[i].getLastName().equals(lastName)) {
+        if (employees.containsKey(s)) {
 
-                this.employees[i].setSalary(newSalary);
-                break;
-
-            }
+            Employee employee = employees.get(s);
+            employee.setSalary(newSalary);
+            employees.put(s, employee);
+            return;
 
         }
+
+        System.out.println("Такого сотрудника нет!");
 
     }
 
     public void changeDepartment(String firstName, String middleName, String lastName, int newDepartment) {
 
-        for (int i = 0; i < count; i++) {
+        String s = firstName + middleName + lastName;
 
-            if (this.employees[i].getFirstName().equals(firstName) && this.employees[i].getMiddleName().equals(middleName)
-                    && this.employees[i].getLastName().equals(lastName)) {
+        if (employees.containsKey(s)) {
 
-                this.employees[i].setDepartment(newDepartment);
-                break;
-
-            }
+            Employee employee = employees.get(s);
+            employee.setDepartment(newDepartment);
+            employees.put(s, employee);
+            return;
 
         }
+
+        System.out.println("Такого сотрудника нет!");
 
     }
 
@@ -100,12 +89,14 @@ public class EmployeeBook {
             System.out.println("Сотрудники отдела номер: " + i);
 
 
-            for (int j = 0; j < count; j++) {
+            for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-                if (i == this.employees[j].getDepartment()) {
+                Employee employee = pair.getValue();
 
-                    System.out.println("Ф.И.О: " + this.employees[j].getLastName() + " " + this.employees[j].getFirstName() +
-                            " " + this.employees[j].getMiddleName());
+                if (i == employee.getDepartment()) {
+
+                    System.out.println("Ф.И.О: " + employee.getLastName() + " " + employee.getFirstName() +
+                            " " + employee.getMiddleName());
                     index++;
 
                 }
@@ -125,11 +116,11 @@ public class EmployeeBook {
 
         double sum = 0;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null) {
-                sum += employee.getSalary();
-            }
+            Employee employee = pair.getValue();
+            sum += employee.getSalary();
+
         }
 
         System.out.println("Общая сумма зарплат равна: " + sum);
@@ -140,15 +131,19 @@ public class EmployeeBook {
 
         double min = Double.MAX_VALUE;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getSalary() < min) {
+            Employee employee = pair.getValue();
+
+            if (employee.getSalary() < min) {
                 min = employee.getSalary();
             }
 
         }
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
+
+            Employee employee = pair.getValue();
 
             if (employee != null && employee.getSalary() == min) {
 
@@ -165,17 +160,21 @@ public class EmployeeBook {
 
         double max = Double.MIN_VALUE;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getSalary() > max) {
+            Employee employee = pair.getValue();
+
+            if (employee.getSalary() > max) {
                 max = employee.getSalary();
             }
 
         }
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getSalary() == max) {
+            Employee employee = pair.getValue();
+
+            if (employee.getSalary() == max) {
 
                 System.out.println("Сотрудник с максимальной зарплатой - " + employee);
                 return;
@@ -191,12 +190,11 @@ public class EmployeeBook {
         double sum = 0;
         int count = 0;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null) {
-                sum += employee.getSalary();
-                count++;
-            }
+            Employee employee = pair.getValue();
+            sum += employee.getSalary();
+            count++;
 
         }
 
@@ -208,12 +206,10 @@ public class EmployeeBook {
 
         System.out.println("Список сотрудников по Ф.И.О: ");
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null) {
-                System.out.println(employee.getLastName() + " " + employee.getFirstName() + " "
-                        + employee.getMiddleName());
-            }
+            Employee employee = pair.getValue();
+            System.out.println(employee.getLastName() + " " + employee.getFirstName() + " " + employee.getMiddleName());
 
         }
 
@@ -223,11 +219,11 @@ public class EmployeeBook {
 
         if (percent < -100) throw new IllegalArgumentException("Индексация меньше -100% не может быть произведена");
 
-        for (int i = 0; i < 10; i++) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (this.employees[i] != null) {
-                this.employees[i].setSalary(this.employees[i].getSalary() + this.employees[i].getSalary() * percent / 100);
-            }
+            Employee employee = pair.getValue();
+            employee.setSalary(employee.getSalary() + employee.getSalary() * percent / 100);
+            employees.put(employee.getFirstName() + employee.getMiddleName() + employee.getLastName(), employee);
 
         }
 
@@ -238,26 +234,24 @@ public class EmployeeBook {
         if (department < 1 || department > 5) throw new IllegalArgumentException("Номер отдела должен быть от 1 до 5");
 
         double min = Double.MAX_VALUE;
-        int index = 0;
+        Employee minSalaryEmployee = null;
 
-        for (int i = 0; i < 10; i++) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (this.employees[i] != null) {
+            Employee employee = pair.getValue();
 
-                if (this.employees[i].getDepartment() == department && this.employees[i].getSalary() < min) {
+            if (employee.getDepartment() == department && employee.getSalary() < min) {
 
-                    min = this.employees[i].getSalary();
-                    index = i;
-
-                }
+                min = employee.getSalary();
+                minSalaryEmployee = employee;
 
             }
 
         }
 
         System.out.println("В отделе номер: " + department + " сотрудник с минимальной зарплатой "
-                + this.employees[index].getLastName() + " " + this.employees[index].getFirstName() +
-                " " + this.employees[index].getMiddleName() + " " + "с зарплатой: " + this.employees[index].getSalary());
+                + minSalaryEmployee.getLastName() + " " + minSalaryEmployee.getFirstName() +
+                " " + minSalaryEmployee.getMiddleName() + " " + "с зарплатой: " + minSalaryEmployee.getSalary());
 
     }
 
@@ -266,26 +260,24 @@ public class EmployeeBook {
         if (department < 1 || department > 5) throw new IllegalArgumentException("Номер отдела должен быть от 1 до 5");
 
         double max = -1;
-        int index = 0;
+        Employee maxSalaryEmployee = null;
 
-        for (int i = 0; i < 10; i++) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (this.employees[i] != null) {
+            Employee employee = pair.getValue();
 
-                if (this.employees[i].getDepartment() == department && this.employees[i].getSalary() > max) {
+            if (employee.getDepartment() == department && employee.getSalary() > max) {
 
-                    max = this.employees[i].getSalary();
-                    index = i;
-
-                }
+                max = employee.getSalary();
+                maxSalaryEmployee = employee;
 
             }
 
         }
 
         System.out.println("В отделе номер: " + department + " сотрудник с максимальной зарплатой "
-                + this.employees[index].getLastName() + " " + this.employees[index].getFirstName() +
-                " " + this.employees[index].getMiddleName() + " " + "с зарплатой: " + this.employees[index].getSalary());
+                + maxSalaryEmployee.getLastName() + " " + maxSalaryEmployee.getFirstName() +
+                " " + maxSalaryEmployee.getMiddleName() + " " + "с зарплатой: " + maxSalaryEmployee.getSalary());
 
     }
 
@@ -295,11 +287,14 @@ public class EmployeeBook {
 
         double sum = 0;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getDepartment() == department) {
+            Employee employee = pair.getValue();
+
+            if (employee.getDepartment() == department) {
                 sum += employee.getSalary();
             }
+
         }
 
         System.out.println("Общая сумма зарплат отдела номер: " + department + " равна: " + sum);
@@ -313,9 +308,11 @@ public class EmployeeBook {
         int count = 0;
         double sum = 0;
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getDepartment() == department) {
+            Employee employee = pair.getValue();
+
+            if (employee.getDepartment() == department) {
 
                 sum += employee.getSalary();
                 count++;
@@ -334,10 +331,15 @@ public class EmployeeBook {
         if (department < 1 || department > 5) throw new IllegalArgumentException("Номер отдела должен быть от 1 до 5");
         if (percent < -100) throw new IllegalArgumentException("Индексация меньше -100% не может быть произведена");
 
-        for (int i = 0; i < 10; i++) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (this.employees[i] != null && this.employees[i].getDepartment() == department) {
-                this.employees[i].setSalary(this.employees[i].getSalary() + this.employees[i].getSalary() * percent / 100);
+            Employee employee = pair.getValue();
+
+            if (employee.getDepartment() == department) {
+
+                employee.setSalary(employee.getSalary() + employee.getSalary() * percent / 100);
+                employees.put(employee.getFirstName() + employee.getMiddleName() + employee.getLastName(), employee);
+
             }
 
         }
@@ -352,13 +354,15 @@ public class EmployeeBook {
 
         System.out.println("Сотруднки отдела номер: " + department);
 
-        for (int i = 0; i < 10; i++) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (this.employees[i] != null && this.employees[i].getDepartment() == department) {
+            Employee employee = pair.getValue();
 
-                System.out.println("Ф.И.О: " + this.employees[i].getLastName() + " " + this.employees[i].getFirstName() + " "
-                        + this.employees[i].getMiddleName() + ", ID: " + employees[i].getId() +
-                        ", зарплата: " + this.employees[i].getSalary());
+            if (employee.getDepartment() == department) {
+
+                System.out.println("Ф.И.О: " + employee.getLastName() + " " + employee.getFirstName() + " "
+                        + employee.getMiddleName() + ", ID: " + employee.getId() +
+                        ", зарплата: " + employee.getSalary());
                 count++;
 
             }
@@ -379,9 +383,11 @@ public class EmployeeBook {
 
         System.out.println("Сотрудники с зарплатой меньше " + salary + ":");
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getSalary() < salary) {
+            Employee employee = pair.getValue();
+
+            if (employee.getSalary() < salary) {
 
                 System.out.println("ID: " + employee.getId() + ", Ф.И.О: " + employee.getLastName() +
                         " " + employee.getFirstName() + " " + employee.getMiddleName() + ", зарплата: " + employee.getSalary());
@@ -405,9 +411,11 @@ public class EmployeeBook {
 
         System.out.println("Сотрудники с зарплатой больше либо равной " + salary + ":");
 
-        for (Employee employee : this.employees) {
+        for (Map.Entry<String, Employee> pair : employees.entrySet()) {
 
-            if (employee != null && employee.getSalary() >= salary) {
+            Employee employee = pair.getValue();
+
+            if (employee.getSalary() >= salary) {
 
                 System.out.println("ID: " + employee.getId() + ", Ф.И.О: " + employee.getLastName() +
                         " " + employee.getFirstName() + " " + employee.getMiddleName() + ", зарплата: " + employee.getSalary());
@@ -424,6 +432,3 @@ public class EmployeeBook {
     }
 
 }
-
-
-
